36929ca41fecbbcd177ddbbd9f94caa9a69aa6f9
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-09-21 14:07:39 -0600

    crear funcion analizar archivo xml
    nuevas funciones y estructura

--- SistemArchivos/SistemaArchivoXML.py
+++ SistemArchivos/SistemaArchivoXML.py
@@ -6,9 +6,22 @@ class SistemaArchivo:
         self.ruta = ruta
 
     def leer_archivo(self):
-        with open(self.ruta, 'r') as archivo:
-            contenido = archivo.read()
-            print("> -----[ Lectura texto XML]-----")
-            print(contenido)
-            print("> -----[ Fin lectura texto XML]-----")
-        return contenido
+        try:
+            with open(self.ruta, 'r') as archivo:
+                contenido = archivo.read()
+                print("# -----[ Lectura texto XML]-----")
+                print(contenido)
+                print("# -----[ Fin lectura texto XML]-----")
+            return contenido
+        except:
+            print("¡¡¡ [ERROR] No se pudo leer el archivo !!!")
+
+    def analizar_xml(self):
+        try:
+            #[1]Leer archivo texto
+            texto_archivo = self.leer_archivo()
+            return texto_archivo
+            #[/1]
+        except:
+            print("¡¡¡ [ERROR] No se pudo analizar el archivo XML !!!")
+            return None
--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from SistemArchivos.SistemaArchivoXML import SistemaArchivo
 #Leer Archivo XML
 ruta = 'G:\\2020\\2020_USAC\\Semestre14(2025)\\IPC2\\1_Laboratorio\\8_PROYECTO2\\IPC2_Proyecto2_201906795\\entrada.xml'
 sistema_archivo = SistemaArchivo(ruta)
-texto_archivo = sistema_archivo.leer_archivo()
+texto_archivo = sistema_archivo.analizar_xml()
+print(texto_archivo)
 
 
 
