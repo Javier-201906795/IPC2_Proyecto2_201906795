a0a6bdca4ba6bfd9be9c00c558a9a2a7568f7153
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-09-21 13:59:32 -0600

    Lectura archivo xml
    lee solo el texto no evalua nada

---
+++ SistemArchivos/SistemaArchivoXML.py
@@ -0,0 +1,12 @@
+from xml.dom.minidom import Document, parse
+
+
+class SistemaArchivo:
+    def __init__(self, ruta):
+        self.ruta = ruta
+
+    def leer_archivo(self):
+        with open(self.ruta, 'r') as archivo:
+            contenido = archivo.read()
+            print("Lectura texto XML")
+        return contenido
