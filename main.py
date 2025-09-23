3abd9ce6d33abc6a72423b096bf6094edd70d8ee
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-09-22 23:31:24 -0600

    modificacion saltos de linea para separar el contenido del resultado

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from SistemArchivos.SistemaArchivoXML import SistemaArchivo
 
 
 #Leer Archivo XML
-ruta = 'G:\\2020\\2020_USAC\\Semestre14(2025)\\IPC2\\1_Laboratorio\\8_PROYECTO2\\IPC2_Proyecto2_201906795\\entrada2.xml'
+ruta = 'G:\\2020\\2020_USAC\\Semestre14(2025)\\IPC2\\1_Laboratorio\\8_PROYECTO2\\IPC2_Proyecto2_201906795\\entrada.xml'
 sistema_archivo = SistemaArchivo(ruta)
 invernaderos = sistema_archivo.segmentar_archivo()
-print("?"*80)
+print("\n"*5,"?"*80)
 invernaderos.desplegar()
 
 
