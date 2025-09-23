4bf964a873d3b709c19726448ee935f2463b4c00
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-09-23 13:11:27 -0600

    obtener datos del invernadero

--- SistemaRiegos/SistemaRiegos.py
+++ SistemaRiegos/SistemaRiegos.py
@@ -16,6 +16,27 @@ class SistemaRiegos():
             print(">>> ciclo: ",i)
             #Obtener invernadero
             invernadero = self.colainvernaderos.Pop()
+            #obtener datos
+            nombre = invernadero.nombre
+            numeroHilera = invernadero.numeroHilera
+            plantasXHilera = invernadero.plantasXHilera
+            ListaPlantas = invernadero.ListaPlantas
+            ListaPlanes = invernadero.ListaPlanes
+            ListaDrones = invernadero.ListaDrones
+            print("\n\n ------------- [Instrucciones] --------------")
+            #Obtener Instrucciones
+            for i in range(0,ListaPlanes.tamano()):
+                if i <=0:
+                    plan = ListaPlanes.primero
+                else:
+                    plan = plan.siguiente
+                #Datos plan
+                nombreplan = plan.valor.nombre
+                colaplan = plan.valor.colaplan
+                print(f'\nNombre plan: {nombreplan}')
+                colaplan.desplegar()
+            print("\n\n ------------- [Fin Instrucciones] --------------")
+
         print("########################## [FIN Sistema Riegos] ############################\n\n\n")
             
             
