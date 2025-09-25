6e9b35f3781584ac40571ef6c64a2aef3373ebd3
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-09-25 11:57:50 -0600

    se elmino el ordenamiento cola
    al parecer no es necesario ordenar las colas (problema de interpretacion del proyecto)

--- SistemaRiegos/SistemaRiegos.py
+++ SistemaRiegos/SistemaRiegos.py
@@ -86,42 +86,42 @@ class SistemaRiegos():
             print("!!! Error al Listar Planes !!!")
 
 
-    def obtenervaloresitem(self, cola, numvalor):
-        for i in range(0, numvalor):
-            if i <= 0:
-                nodo = cola.primero
-            else:
-                nodo = nodo.siguiente
+    # def obtenervaloresitem(self, cola, numvalor):
+    #     for i in range(0, numvalor):
+    #         if i <= 0:
+    #             nodo = cola.primero
+    #         else:
+    #             nodo = nodo.siguiente
         
-        return nodo
+    #     return nodo
             
     
     
-    def ordenarcola(self, cola):
-        print('--------------- [{ Ordenamiento COLA }]----------------')
-        cola.desplegar()
-        print('---------\n')
-        nuevacola = Cola()
-        #Ordenamiento burbuja
-        for i in range(0,cola.tamano() -1):
-            for j in range(0,cola.tamano() - i -1):
-                # Si el número actual es mayor que el siguiente, los intercambiamos
-                prim = self.obtenervaloresitem(cola,j+1)
-                sig = self.obtenervaloresitem(cola,j+2)
+    # def ordenarcola(self, cola):
+    #     print('--------------- [{ Ordenamiento COLA }]----------------')
+    #     cola.desplegar()
+    #     print('---------\n')
+    #     nuevacola = Cola()
+    #     #Ordenamiento burbuja
+    #     for i in range(0,cola.tamano() -1):
+    #         for j in range(0,cola.tamano() - i -1):
+    #             # Si el número actual es mayor que el siguiente, los intercambiamos
+    #             prim = self.obtenervaloresitem(cola,j+1)
+    #             sig = self.obtenervaloresitem(cola,j+2)
                 
-                primdato = int(prim.valor.planta[1])
-                sigdato = int(sig.valor.planta[1])
-                if  primdato > sigdato:
-                    #temporal = prim
-                    temporal = CAsignacionPlan(prim.valor.hilera,prim.valor.planta)
-                    #prim = sig (cambiar valor)
-                    prim.valor.asignarplanta(f'P{sigdato}')
-                    #sig = temporal
-                    sig.valor.asignarplanta(temporal.planta)
-                prim.valor.desplegar()
-                sig.valor.desplegar()
+    #             primdato = int(prim.valor.planta[1])
+    #             sigdato = int(sig.valor.planta[1])
+    #             if  primdato > sigdato:
+    #                 #temporal = prim
+    #                 temporal = CAsignacionPlan(prim.valor.hilera,prim.valor.planta)
+    #                 #prim = sig (cambiar valor)
+    #                 prim.valor.asignarplanta(f'P{sigdato}')
+    #                 #sig = temporal
+    #                 sig.valor.asignarplanta(temporal.planta)
+    #             prim.valor.desplegar()
+    #             sig.valor.desplegar()
 
-        print('--------------- [{ Fin Ordenamiento COLA }]----------------')
+    #     print('--------------- [{ Fin Ordenamiento COLA }]----------------')
 
 
 
@@ -203,10 +203,10 @@ class SistemaRiegos():
                 
                 print('-cola a ordenar-')
                 ColaHilera.desplegar()
-                #Ordenar cola
-                self.ordenarcola(ColaHilera)
-                print('-Nuevo orden-')
-                ColaHilera.desplegar()
+                # #Ordenar cola
+                # self.ordenarcola(ColaHilera)
+                # print('-Nuevo orden-')
+                # ColaHilera.desplegar()
                 
                 
                 #Almacenar hilera
