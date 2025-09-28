dea5a8f8d59c0e17e8b112c8e8cb0b5c52a34e78
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-09-28 16:49:45 -0600

    se elimino la funcion subir archivo ya que ahora se mane en el index

--- app.py
+++ app.py
@@ -63,36 +63,6 @@ def inicio():
 def resumen():
     return render_template('salidaH.html')
 
-@app.route('/subirarchivo', methods=['GET','POST'])
-def subirarchivo():
-    try:
-        print('> /subirarchivo')
-        if request.method == 'POST':
-            print('>> Reciviendo archivo')
-            #Evaluar Archivo
-            mensaje = ''
-            if 'archivo' not in request.files:
-                mensaje = '!! No se encontro el archivo !! '
-                print(mensaje)
-            #Archivo
-            archivo = request.files['archivo']
-            if archivo.filename == '':
-                mensaje = '!! No se selecciono ningun archivo !!'
-                print(mensaje)
-            
-            #Comprobar
-            if mensaje == '':
-                #Guardar Archivo
-                ruta_archivo = os.path.join(os.getcwd(),'entrada.xml')
-                archivo.save(ruta_archivo)
-                print('>>> Se subio el archivo correctamente.')
-                app.config['banderaArchivonuevo'] = True
-            
-            return redirect(url_for('inicio'))
-        elif request.method == 'GET':
-            return render_template('subirArchivo.html')
-    except Exception as e:
-        print('!!! Error en subirarchivo() !!!\n',e)
 
 def corresistemacentral():
     print('>>> Inicio sistema central')
