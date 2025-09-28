6304b54ded3bec564ae4c894905a9e7b4fba06e8
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-09-28 16:54:36 -0600

    boton ver resumen
    muestar el resumen html creado

--- SistemArchivos/SistemaArchivoSalidaHTML.py
+++ SistemArchivos/SistemaArchivoSalidaHTML.py
@@ -297,7 +297,7 @@ class SistemaArchivoHTML:
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Invernaderos — Plan Semana 3</title>
+  <title>Invernaderos</title>
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -327,7 +327,7 @@ class SistemaArchivoHTML:
   <!-- NAVBAR -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container">
-      <a class="navbar-brand" href="#">Invernaderos</a>
+      <a class="navbar-brand" href="/">Sistema Guate Riegos 2.0</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMain">
         <span class="navbar-toggler-icon"></span>
       </button>
